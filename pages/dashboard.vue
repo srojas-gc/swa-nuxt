64370188fef4d0135e5effad7ea4f2cd1d9a2e4c
srojas-gc <srojas@cloudspace.com.ar>
2021-09-15 10:30:57 -0300

    init app

---
+++ pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/logoGC.png"
+        alt="Dashboard CloudSpace"
+        class="mb-5"
+        style="width:400px"
+      >
+      <blockquote class="blockquote">
+        &#8220;Dashboard Admin for Grupo CloudSpace Template&#8221;
+        <footer>
+          <small>
+            <em>&mdash;srojas</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
