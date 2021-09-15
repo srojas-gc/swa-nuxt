5f76c7acbb2ed451883291b6d1c6ddfbf84b92ae
srojas-gc <srojas@cloudspace.com.ar>
2021-09-15 10:53:26 -0300

    SWA-16:testing amplify integration on nuxt-template

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,19 +6,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxt-site',
-    title: 'nuxt-site',
+    titleTemplate: '%s - CloudSpace',
+    title: 'Dashboard',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Dashboard Template - CloudSpace' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
     ]
   },
 
